26ddddfb15da8b40fff2485a6a1a17b0772b316e
pamado98 <amado_teste@hotmail.com>
2025-05-15 02:19:24 +0100

    app.py

--- app.py
+++ app.py
@@ -110,16 +110,17 @@ if st.button("Ver resultado"):
     fig, ax = plt.subplots(figsize=(6,6))
     ax.scatter(eixo_econ, eixo_soc, color="black", s=120)
     ax.text(eixo_econ + 0.2, eixo_soc, "Estás aqui!", fontsize=9)
-    for _, row in df.iterrows():
+   
+for _, row in df.iterrows():
     partido_id = row['partido'].lower().replace(" ", "_").replace("(", "").replace(")", "").replace("+", "").replace("!", "").replace(".", "").replace("-", "").replace("/", "")
     logo_path = os.path.join("logos", f"{partido_id}.png")
 
-        if os.path.exists(logo_path):
+    if os.path.exists(logo_path):
         img = plt.imread(logo_path)
         imagebox = OffsetImage(img, zoom=0.12)
         ab = AnnotationBbox(imagebox, (row['econ'], row['soc']), frameon=False)
         ax.add_artist(ab)
-        else:
+    else:
         ax.scatter(row['econ'], row['soc'], s=80)
         ax.text(row['econ'] + 0.2, row['soc'], row['partido'], fontsize=8)
 
